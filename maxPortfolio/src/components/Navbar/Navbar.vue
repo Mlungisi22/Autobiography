6e308cb74e9b35820dafc91b199016a62801f24c
Mlungisi Maxwell Mbuyisa <mlungisi@synthesis.co.za>
2024-07-30 23:29:04 +0200

    fixed logo and navbar styling

--- maxPortfolio/src/components/Navbar/Navbar.vue
+++ maxPortfolio/src/components/Navbar/Navbar.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="navbar">
-    <div class="navbar left">
-      <img src="../../assets/Mlogo.png" style="height: 50px" />
+    <div class="navbar-left">
+      <img src="../../assets/myicon.png" style="height: 40px" />
+    </div>
+    <div class="navbar-middle">
+      <div class="nav-tab">Home</div>
+      <div class="nav-tab">About</div>
+      <div class="nav-tab">Contact</div>
+      <div class="nav-tab" @click="showprojects">Projects</div>
     </div>
     <div class="navbar-right">
-      <span class="nav-tab"> Home </span>
-      <span class="nav-tab"> About </span>
-      <span class="nav-tab"> Contact </span>
-      <span class="nav-tab" @click="showprojects"> Projects </span>
+      <span>x</span>
     </div>
   </div>
 </template>
@@ -16,24 +19,48 @@
 import { useRouter } from 'vue-router'
 const router = useRouter()
 const showprojects = () => {
-  router.push('/about');
+  router.push('/about')
 }
 </script>
 <style scoped>
 .navbar {
   display: flex;
   flex-direction: row;
-  background-color: black;
+  background-color: rgb(255, 255, 255);
   color: aliceblue;
   align-items: center;
+  justify-content: space-between;
   top: 0;
+}
+.nav-tab {
+  color: black;
+  width: 150px;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+}
+
+.navbar-middle :hover{
+  color: aliceblue;
+  background-color: black;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.navbar-left {
+  width: 15vw;
 }
 
 .navbar-right {
-  width: 80vw;
+  width: 15vw;
+  display: flex;
+  align-items: center;
+}
+
+.navbar-middle {
   height: 40px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
 }
 </style>
